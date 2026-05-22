0e9e6677203cd860b3fa36d8583a8b044069276a
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-22 14:17:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,8 @@ import streamlit as st
 from numpy.random import default_rng as rng
 
 df = pd.DataFrame(
-    {
-        "col1": list(range(20)) * 3,
-        "col2": rng(0).standard_normal(60),
-        "col3": ["a"] * 20 + ["b"] * 20 + ["c"] * 20,
-    }
+    rng(0).standard_normal((1000, 2)) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
 )
 
-st.area_chart(df, x="col1", y="col2", color="col3")
+st.map(df)
