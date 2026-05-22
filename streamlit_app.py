dc44dd476b70770d52480594b54ade28a3e4d580
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-22 14:15:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,16 @@ st.header("Four", divider=True)
 df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
 
 st.area_chart(df)
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(
+    {
+        "col1": list(range(20)) * 3,
+        "col2": rng(0).standard_normal(60),
+        "col3": ["a"] * 20 + ["b"] * 20 + ["c"] * 20,
+    }
+)
+
+st.area_chart(df, x="col1", y="col2", color="col3")
