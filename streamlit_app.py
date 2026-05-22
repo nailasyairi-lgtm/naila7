409d1e2550b9db48d8963cf0949555d3a3b46f5a
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-22 14:21:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,6 @@ st.title("Aplikasi asya")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import time
 import streamlit as st
 
-with st.status("Downloading data..."):
-    st.write("Searching for data...")
-    time.sleep(2)
-    st.write("Found URL.")
-    time.sleep(1)
-    st.write("Downloading data...")
-    time.sleep(1)
-
-st.button("Rerun")
+st.warning('This is a warning', icon="⚠️")
