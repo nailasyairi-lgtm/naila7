cb429fc0804b5c4db03d7edc654ba263c9f3446b
nailasyairi-lgtm <nailasyairi@gmail.com>
2026-05-22 14:13:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,8 @@ st.title("Aplikasi asya")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
